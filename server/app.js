fb7c93f86e55a966477a01a3f2dcf94d602435f8
Leslie <lesore0789@gmail.com>
2023-03-22 23:27:15 -0700

    Phase 1 and 2 complete

--- server/app.js
+++ server/app.js
@@ -25,6 +25,24 @@ const express = require('express');
 const app = express();
 
 // Your code here
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Request Body:', req.body);
+  next();
+});
+
+app.get("/artists", (req, res) => {
+  res.statusCode = 200;
+
+  res.send(getAllArtists());
+});
+
+app.post("/artists", (req, res) => {
+  res.statusCode = 201;
+
+  res.send(addArtist(req.body))
+})
 
 // DO NOT MODIFY
 if (require.main === module) {
