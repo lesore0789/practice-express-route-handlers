2929c3a5fb4c9eb0010f6ae27ece0c721a2744df
Leslie <lesore0789@gmail.com>
2023-03-24 00:47:14 -0700

    Long Practice complete

--- server/app.js
+++ server/app.js
@@ -56,6 +56,117 @@ app.get("/artists/latest/albums", (req, res) => {
   res.send(getAlbumsForLatestArtist())
 });
 
+// Long Practice
+
+// Get a specific artist's details based on artistId
+app.get("/artists/:artistId", (req, res) => {
+  const artistId = req.params.artistId;
+
+  res.send(getArtistByArtistId(artistId))
+})
+
+// Edit a specified artist by artistId
+app.put('/artists/:artistId', (req, res) => {
+  let data = req.body;
+  const artistId = req.params.artistId;
+
+  res.send(editArtistByArtistId(artistId, data));
+});
+
+// Delete a specified artist by artistId
+app.delete('/artists/:artistId', (req, res) => {
+  let artistId = req.params.artistId;
+  const message = {
+    "message": `Successfully deleted`
+  };
+  deleteArtistByArtistId(artistId);
+  res.send(message)
+})
+
+// Get all albums of a specific artist based on artistId
+app.get('/artists/:artistId/albums', (req, res) => {
+  let artistId = req.params.artistId;
+  res.send(getAlbumsByArtistId(artistId));
+});
+
+// Get a specific album's details based on albumId
+app.get('/albums/:albumId', (req, res) => {
+  let albumId = req.params.albumId;
+  res.send(getAlbumByAlbumId(albumId))
+});
+
+// Add an album to a specific artist based on artistId
+app.post('/artist/:artistId/albums', (req, res) => {
+  let artistId = req.params.artistId;
+  let data = req.body;
+  res.status(201).send(addAlbumByArtistId(artistId, data))
+})
+
+// Edit a specified album by albumId
+app.put('/albums/:albumId', (req, res) => {
+  let albumId = req.params.albumId;
+  let data = req.body;
+  res.send(editAlbumByAlbumId(albumId, data))
+});
+
+// Delete a specified album by albumId
+app.delete('/albums/:albumId', (req, res) => {
+  let albumId = req.params.albumId;
+  let message = {
+    "message": "Successfully deleted"
+  };
+  deleteAlbumByAlbumId(albumId);
+  res.send(message);
+})
+
+// Get all albums with names filtered by first letter
+app.get('/albums', (req, res) => {
+  let query = req.query.startsWith;
+  res.send(getFilteredAlbums(query))
+});
+
+// Get all songs of a specific artist based on artistId
+app.get('/artists/:artistId/songs', (req, res) => {
+  let artistId = req.params.artistId;
+  res.send(getSongsByArtistId(artistId))
+});
+
+// Get all songs of a specific album based on albumId
+app.get('/albums/:albumId/songs', (req, res) => {
+  let albumId = req.params.albumId;
+  res.send(getSongsByAlbumId(albumId));
+});
+
+// Get a specific song's details based on songId
+app.get('/songs/:songId', (req, res) => {
+  let songId = req.params.songId;
+  res.send(getSongBySongId(songId));
+});
+
+// Add a song to a specific album based on albumId
+app.post('/albums/:albumId/songs', (req, res) => {
+  let albumId = req.params.albumId;
+  let data = req.body;
+  res.status(201).send(addSongByAlbumId(albumId, data));
+});
+
+// Edit a specified song by songId
+app.put('/songs/:songId', (req, res) => {
+  let songId = req.params.songId;
+  let data = req.body;
+  res.send(editSongBySongId(songId, data));
+});
+
+// Delete a specified song by songId
+app.delete('/songs/:songId', (req, res) => {
+  let songId = req.params.songId;
+  let message = {
+    "message": "Successfully deleted"
+  };
+  deleteSongBySongId(songId);
+  res.send(message)
+})
+
 // DO NOT MODIFY
 if (require.main === module) {
   const port = 8000;
